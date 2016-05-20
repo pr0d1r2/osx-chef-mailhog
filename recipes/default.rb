9b872ce2bda85ed294c3dbc85c5032ae02bd2974
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-20 17:52:11 +0200

    Support for chefdk kitchenplan config

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,29 @@
-include_recipe "homebrewalt::default"
+if node['user'] && node['user']['id']
+  include_recipe "homebrew::default"
+  user_name = node['user']['id']
+  home_dir = Etc.getpwnam(user_name).dir
+else
+  include_recipe "homebrewalt::default"
+  user_name = node['current_user']
+  home_dir = node['etc']['passwd'][user_name]['dir']
+end
 
 ["homebrew.mxcl.mailhog.plist" ].each do |plist|
-  plist_path = File.expand_path(plist, File.join('~', 'Library', 'LaunchAgents'))
+  plist_path = File.expand_path(plist, File.join(home_dir, 'Library', 'LaunchAgents'))
   if File.exists?(plist_path)
     log "mailhog plist found at #{plist_path}"
     execute "unload the plist (shuts down the daemon)" do
       command %'launchctl unload -w #{plist_path}'
-      user node['current_user']
+      user user_name
     end
   else
     log "Did not find plist at #{plist_path} don't try to unload it"
   end
 end
 
-[ "/Users/#{node['current_user']}/Library/LaunchAgents" ].each do |dir|
+[ "#{home_dir}/Library/LaunchAgents" ].each do |dir|
   directory dir do
-    owner node['current_user']
+    owner user_name
     action :create
   end
 end
@@ -26,12 +34,12 @@ end
 
 execute "copy over the plist" do
   command %'cp /usr/local/opt/mailhog/homebrew.mxcl.mailhog.plist ~/Library/LaunchAgents/'
-  user node['current_user']
+  user user_name
 end
 
 execute "start the daemon" do
   command %'launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mailhog.plist'
-  user node['current_user']
+  user user_name
 end
 
 ruby_block "Checking that mailhog is running" do
